179ab44af858ac7426916b1bd098a12be18c6fb3
nodesrk <67991569+sypase@users.noreply.github.com>
2024-08-26 16:56:49 +0545

    Fixed: Verification

--- test.js
+++ test.js
@@ -1,5 +1,34 @@
+// import bitmoroService from "bitmoro";
+
+// let bitmoro = new bitmoroService("ef4ff9305a64cfeb6325a57747cfba760463248fbba1d92efd5adcfa1518");
+
+// // // Test sendMessage
+// // let data = bitmoro.sendMessage("Hello", ["9841452888"]);
+// // console.log(data);
+
+// // Test sendOtp
+// let otp = await bitmoro.sendOtp(["9841452888"]);
+// console.log(otp);
+
+// // // Test verifyOtp
+// // let isValid = bitmoro.verifyOtp("9841452888", otp);
+// // console.log(isValid);
+
+// // // Test resendOtp
+// // let newOtp = bitmoro.resendOtp("9841452888");
+// // console.log(newOtp);
+
 import bitmoroService from "bitmoro";
 
-let bitmoro= new bitmoroService("f5012326ed00c2ca6293f4166b72e124908f62b7b154d3ae8efd24ffb8ec");
-let data=bitmoro.sendMessage("Hello",["9841452888"]);
-console.log(data);
+async function test()
+{let bitmoro= new bitmoroService("ef4ff9305a64cfeb6325a57747cfba760463248fbba1d92efd5adcfa1518");
+    let data= await bitmoro.sendMessage("Hello",["9869363132"]);
+    let otp= await bitmoro.sendOtp(["9869363132"],30);
+    let verify=bitmoro.verifyOtp(["9869363132"],otp);
+    if(verify)
+    {
+        console.log("verified")
+    }
+
+}
+test();
